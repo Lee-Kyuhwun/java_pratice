abc133de856489e0512824aaecaeec768e3c0220
이규훈 <dlrbgns0904@sju.ac.kr>
2024-05-22 21:52:10 +0900

    feat: 자바 접근제어자 실습

--- java-basic/src/access/Speaker.java
+++ java-basic/src/access/Speaker.java
@@ -1,4 +1,35 @@
 package access;
 
 public class Speaker {
+
+    private int volume;
+
+    Speaker(int volume){
+        this.volume = volume;
+    }
+
+    void volumeUp(){
+        if(volume >= 100){
+            System.out.println("볼륨을 더 높일 수 없습니다.");
+        }
+        else{
+            volume +=10;
+
+            System.out.println("현재 볼륨 : " + volume);
+        }
+    }
+
+    void volumeDown(){
+        if(volume <= 0){
+            System.out.println("볼륨을 더 낮출 수 없습니다.");
+        }
+        else{
+            volume -=10;
+            System.out.println("현재 볼륨 : " + volume);
+        }
+    }
+
+    void showVolume(){
+        System.out.println("현재 볼륨 : " + volume);
+    }
 }
