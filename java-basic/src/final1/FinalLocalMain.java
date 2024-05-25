baae7b03541033fdfe1520eebd525bc2e2d834e1
steve Lee <steve.lee@obigo.com>
2024-05-25 13:15:01 +0900

    feat: 자바 기본 final 실습

---
+++ java-basic/src/final1/FinalLocalMain.java
@@ -0,0 +1,23 @@
+package final1;
+
+public class FinalLocalMain {
+
+    public static void main(String[] args) {
+        // final ㅣㅈ역변수
+
+        final int data1;
+
+        data1 =10;
+
+        final int data2 = 10;
+//        data2= 20;
+
+
+
+    }
+
+    static void method (final int parameter){
+        //parameter = 20;
+
+    }
+}
