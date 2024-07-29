7dce762a96a6d4f5e49e35efc2fba3d88d427bcd
LeeKyuHwun <dlrbgns0904@sju.ac.kr>
2024-07-29 19:22:19 +0900

    feat: 자바 상속 실습

---
+++ java-basic/src/casting/Child.java
@@ -0,0 +1,7 @@
+package casting;
+
+public class Child extends Parent {
+    public void childMethod() {
+        System.out.println("Child.childMethod");
+    }
+}
---
+++ java-basic/src/casting/Parent.java
@@ -0,0 +1,7 @@
+package casting;
+
+public class Parent {
+    public void parentMethod() {
+        System.out.println("Parent.parentMethod");
+    }
+}
