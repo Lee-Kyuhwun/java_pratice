3c361747588370bc2b0eaa273e435aff3abf84de
이규훈 <dlrbgns0904@sju.ac.kr>
2024-12-27 16:59:35 +0900

    feat: 다형성 예제

---
+++ java-basic/src/final1/ConstantMain.java
@@ -0,0 +1,17 @@
+package final1;
+
+public class ConstantMain {
+    public static void main(String[] args) {
+
+
+        System.out.println("1000 = " + 1000);
+
+
+
+    }
+
+
+    private static void process(int current) {
+        System.out.println("current = " + current);
+    }
+}
