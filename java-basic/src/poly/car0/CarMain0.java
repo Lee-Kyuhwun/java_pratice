efcd4eef7db98de3fee930961c76018bb628b484
이규훈 <dlrbgns0904@sju.ac.kr>
2024-12-27 17:10:52 +0900

    feat: 다형성-역할과 구현 예제2

--- java-basic/src/poly/car0/CarMain0.java
+++ java-basic/src/poly/car0/CarMain0.java
@@ -5,7 +5,15 @@ public class CarMain0 {
     public static void main(String[] args) {
         Driver driver = new Driver();
         K3Car k3Car = new K3Car();
+
         driver.setK3Car(k3Car);
+        driver.drive();
+
+
+        // model3 변경
+        Model3Car model3Car = new Model3Car();
+        driver.setModel3Car(model3Car);
+        driver.setK3Car(null);
         driver.drive();
     }
 
